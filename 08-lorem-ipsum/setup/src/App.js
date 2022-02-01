9ee53922ae79d4c2f3c280cba682181fb8eb5435
RafaelMolyna <rafaelogallas@gmail.com>
2022-02-01 19:26:36 -0300

    lesson 08 were done

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,10 +1,42 @@
 import React, { useState } from 'react';
 import data from './data';
+
+
 function App() {
-  
-  
+  const [count, set_count] = useState(0);
+  const [text, set_text] = useState(0);
+
+  const handleSubmit = function(e) {
+    e.preventDefault();
+    let hipsterParag = count;
+    if (hipsterParag < 0) {
+      hipsterParag = 1;
+    } 
+    set_text(hipsterParag);
+  };
+
   return (
-  <h2>lorem ipsum project setup</h2>
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className='lorem-form' onSubmit={handleSubmit} >
+          <label htmlFor="amount">
+            hipster paragraphs:
+          </label>
+          <input type="number"
+                name='amount'
+                id='amount'
+                value={count}
+                onChange={(e) => {set_count(e.target.value)}} />
+        <button type='submit' className='btn' >
+          generate it:
+        </button>
+      </form>
+      {data.slice(0, text).map(paragraph => {
+        return (
+          <p>{paragraph} </p>
+        );
+      })}
+    </section>
     )
 }
 
